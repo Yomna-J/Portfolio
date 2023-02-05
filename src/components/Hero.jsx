827df82b30e6fc932f747b17a0262f006de3bb60
Yomna-J <youmnajaza@gmail.com>
2023-02-05 23:37:10 +0300

    Modified Hero     - Fixed Hero height

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,7 @@ function addEffectWriting(element, time) {
   return (
     <div
       id="about"
-      className="mx-auto flex flex-col  justify-center p-4 text-center md:flex-row md:items-center md:justify-between md:text-left lg:max-w-7xl"
+      className="mx-auto flex h-[90vh] flex-col justify-center px-4 py-1 text-center md:flex-row md:items-center md:justify-between md:text-left lg:max-w-7xl"
     >
       <div className="py-4 md:w-1/2 md:flex-col">
         <h1 className="py-1 text-5xl font-black text-primary md:text-7xl">
